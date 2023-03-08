372b2f176ba1e8f5f035820e21433d608141ce75
Jarik Henriksen <59142449+jarikhenriksen@users.noreply.github.com>
2023-03-07 19:53:23 -0500

    migrate pet model, create html template and index

--- djangoPractice/petShop/views.py
+++ djangoPractice/petShop/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from petShop.models import Pet
 
 def index(request):
-    return HttpResponse('Hello world')
+
+    allPets = Pet.objects.all()
+
+    return render(request, 'petShop/index.html')
 
