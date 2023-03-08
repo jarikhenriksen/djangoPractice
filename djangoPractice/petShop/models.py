02b9b4e88322d68f108cf307aa1ff54f014004bb
Jarik Henriksen <59142449+jarikhenriksen@users.noreply.github.com>
2023-03-07 19:38:02 -0500

    add models for pets

--- djangoPractice/petShop/models.py
+++ djangoPractice/petShop/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Pet(models.Model):
+    name = models.CharField(max_length=50)
+    colour = models.CharField(max_length=50)
+    species = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+        
+
