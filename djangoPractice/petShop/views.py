30c90a3062b26b999ef68aef7e8d457211312ad7
Jarik Henriksen <59142449+jarikhenriksen@users.noreply.github.com>
2023-03-07 20:24:18 -0500

    added individual animal page

--- djangoPractice/petShop/views.py
+++ djangoPractice/petShop/views.py
@@ -2,9 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from petShop.models import Pet
 
+
+
 def index(request):
 
     allPets = Pet.objects.all()
+    
+    return render(request, 'petShop/index.html', {
+        'allPets': allPets
+    })
 
-    return render(request, 'petShop/index.html')
+
+def petPage(request, name):
+
+    specificPet = Pet.objects.get(name = name)
+    
+    return render(request, 'petShop/specificPet.html', {
+        'pet': specificPet
+    })
 
